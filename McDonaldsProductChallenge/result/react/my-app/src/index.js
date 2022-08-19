fef36faa36be11378317fd38c23d27eae8dea33a
AbelMurgas <abelmurgasdev@gmail.com>
2022-08-19 11:15:21 -0500

    created structure

---
+++ McDonaldsProductChallenge/result/react/my-app/src/index.js
@@ -0,0 +1,14 @@
+// Bootstrap CSS
+import "bootstrap/dist/css/bootstrap.min.css";
+// Bootstrap Bundle JS
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
